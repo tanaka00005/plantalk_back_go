06f6c0d23efa7c05fdcc37490535c0fe4090b525
tanaka00005 <x23054xx@aitech.ac.jp>
2025-05-15 19:54:51 +0900

    新規登録の実装

--- login/main.go
+++ login/main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"os"
+	"log"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/joho/godotenv"
 	"github.com/tanaka00005/plantalk_back_go/login/token"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	Email string `json:"email" binding:"required"`
+	Email string `json:"email" binding:"required" gorm:"uniqueIndex;size:255"`
 	Name string `json:"name" binding:"required"`
 	Password string `json:"password" binding:"required"`
+	ID	uint	`json:"id" gorm:"primaryKey;autoIncrement"`
 }
 
 func main(){
-	r := gin.Default()
 
+
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+	
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbScheema := os.Getenv("DB_SCHEEMA")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	fmt.Printf("dbUsername:%s\n",dbUsername)
+	fmt.Printf("dbPort:%s\n",dbPort)
+	fmt.Printf("dbScheema:%s\n",dbScheema)
+	fmt.Printf("dbPassword:%s\n",dbPassword)
+
+	
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",dbUsername,dbPassword,dbPort,dbScheema)
+	//dsn := "root:password@tcp(127.0.0.1:53306)/plantalk_go?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+		panic("failed to connect database")
+	}
+	//テーブルのマイグレーション
+	db.AutoMigrate(&User{})
+	r := gin.Default()
+	
 	r.GET("/",func (c *gin.Context)  {
 		c.JSON(200,gin.H{
 			"message":"Hello world",
@@ -34,12 +67,14 @@ func main(){
 
 		// パスワードのハッシュ化
 		//soltはランダムな文字
-		hash,err := EncryptPassword("password")
+		hash,err := EncryptPassword(user.Password)
 
 		if err != nil{
 			panic(err)
 		}
 		fmt.Println(hash)
+
+		user.Password = hash
 
 		// compareResult := CompareHashAndPassword(hash,"password")
 
@@ -50,6 +85,12 @@ func main(){
 		// }
 
 		accessToken,err := token.Token()
+
+		result := db.Create(&user)
+
+		if result.Error != nil {
+			panic("failed to insert record")
+		}
 
 		if err != nil{
 			panic(err)
@@ -67,6 +108,7 @@ func main(){
 		}
 
 		
+
 	})
 
 	r.Run(":8080")
@@ -84,6 +126,9 @@ func CompareHashAndPassword(hash, password string) bool {
 
 	return err == nil
 }
+
+
+
 
 
 
